e80e312ffa37cde353841037ebf8f53dc6b4baea
GUDESARROLLOS <admgudesarrollos@gmail.com>
2024-04-25 15:13:13 -0300

    .

--- script/scripts.js
+++ script/scripts.js
@@ -3,20 +3,22 @@ const buttons= document.querySelectorAll("button");
 
  
 
-
 buttons.forEach((btn) => {
     btn.addEventListener("click", () =>{
         if(btn.id === "="){
-        document.getElementById("total").innerHTML =  display.value = eval(display.value);
-        total.toLocaleString("en-ES")
+        document.getElementById("total").innerHTML = display.value = eval(display.value);
         } else if (btn.id === "ac"){
             document.getElementById("total").innerHTML =  display.value = "";
         } else if (btn.id == "de"){
             display.value = display.value.slice(0, -1);
         } else { 
-             display.value += btn.id
-        }})  
+            display.value += btn.id;
+        }}) 
+        
 })
+
+
+
     
 function startTime(){
     today=new Date();
