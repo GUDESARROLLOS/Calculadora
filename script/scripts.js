e9f8a7914286e16b4af13800706d0ba8918adbb6
GUDESARROLLOS <admgudesarrollos@gmail.com>
2024-04-26 18:02:23 -0300

    .

--- script/scripts.js
+++ script/scripts.js
@@ -5,13 +5,16 @@ var bc = 10400;
 
 
 
+
+
 buttons.forEach((btn) => {
     btn.addEventListener("click", () =>{
         if(btn.id === "="){ 
-     document.getElementById("total").innerHTML = display.value = Math.round(eval(display.value * bc));
-           
+         document.getElementById("total").innerHTML  = display.value = Math.round(eval(display.value * bc))
+         document.getElementById("style").style.display = 'block';
     } else if (btn.id === "ac"){
-            document.getElementById("total").innerHTML =  display.value = "";
+            document.getElementById("total").innerHTML =  display.value = ""
+            document.getElementById("style").style.display = 'none';
         } else if (btn.id == "de"){
             display.value = display.value.slice(0, -1);
         } else { 
@@ -20,7 +23,10 @@ buttons.forEach((btn) => {
     })   
 })
 
+
+
     
+
 function startTime(){
     today=new Date();
     h=today.getHours();
@@ -32,4 +38,11 @@ function startTime(){
     {if (i<10) {i="0" + i;}return i;}
     window.onload=function(){startTime();}
 
-    
+
+ function change(){
+    fristext = document.getElementsByClassName("textPrimery");
+    secondtext = document.getElementsByClassName("textSecondary");
+
+    secondtext.get
+
+ }
